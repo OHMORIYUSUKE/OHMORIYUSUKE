1e98d332bb762051e39c9e0a3e8af7a2c745f68b
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-09-13 16:35:06 +0900

    Fix

---
+++ worksDataGetTime.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "time"
+	"os"
+	"log"
+	"regexp"
+	"strings"
+)
+
+func main(){
+	now := time.Now()
+
+    nowUTC := now.UTC() 
+
+    jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+
+    nowJST := nowUTC.In(jst)
+
+	nowJST.Format("2006-01-02 15:04:05")
+
+	//-----------------------
+	f, err := os.Open("README.md")
+    if err != nil{
+        fmt.Println("error")
+    }
+    defer f.Close()
+
+    // 一気に全部読み取り
+    b, err := ioutil.ReadAll(f)
+
+	str := []byte(string(b))
+	assigned := regexp.MustCompile("<!--works-GetDtataTime-->\n\n(.*)\n\n<!--works-GetDtataTime-->")
+	group := assigned.FindSubmatch(str)
+	//fmt.Println(string(group[1])) 
+
+	replacedMd := strings.Replace(string(b), string(group[1]), "最終更新 : " + nowJST.Format("2006-01-02 15:04:05"), 1)
+	//fmt.Println(replacedMd)
+
+	file, err := os.Create("README.md")
+	if err != nil {
+		log.Fatal(err)  //ファイルが開けなかったときエラー出力
+	}
+	defer file.Close()
+
+	file.Write(([]byte)(replacedMd))
+}
