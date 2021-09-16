ede8498359706dffaf67c60577b19ab81ee960e1
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-09-16 10:13:47 +0900

    sh

---
+++ blend.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# aptアップデート,snapインストール,Blenderインストール
+sudo apt -y update
+sudo apt list --upgradable
+sudo apt install snapd
+sudo snap install blender --classic
+
+# Blenderをレンダリング
+# https://docs.blender.org/manual/en/latest/advanced/command_line/arguments.html
+sudo blender --background -noaudio blend/Miraikomachi.blend --threads 0 -E CYCLES --render-output //anim --render-frame 2000
