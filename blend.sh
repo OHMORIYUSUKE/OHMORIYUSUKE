1bf944b21f754001888e914b309839e789abb822
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-09-16 10:31:58 +0900

    C:/Program Files/Git/img

--- blend.sh
+++ blend.sh
@@ -8,4 +8,5 @@ sudo snap install blender --classic
 
 # Blenderをレンダリング
 # https://docs.blender.org/manual/en/latest/advanced/command_line/arguments.html
-sudo blender --background -noaudio blend/Miraikomachi.blend --threads 0 -E CYCLES --render-output //anim --render-frame 2000
+sudo blender --background -noaudio blend/Miraikomachi.blend --threads 0 -E CYCLES --render-output /img //anim --render-frame 2000
+#                                                                                                   |          
